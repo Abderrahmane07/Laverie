e5629beed2ceb089d144412913eb1fbfcc34a785
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2023-08-06 06:41:49 +0200

    closer look to the prototype

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,16 +6,24 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Laverie"),
-      ),
-      body: const Center(
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
+            Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey,
+              child: const Center(child: Text('ad zone')),
+            ),
+            const Text(
               'Laverie details here',
+            ),
+            Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey,
+              child: const Center(child: Text('ad zone')),
             ),
           ],
         ),
