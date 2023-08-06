833c80f88336ad35d9f665245adfa1a7cb0b2d88
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2023-08-06 06:35:07 +0200

    better separation and simplification

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Laverie',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
