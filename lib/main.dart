26b695ffc2644b7292d101973a96b6dcc1a57c72
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2023-08-07 03:23:19 +0200

    manage the linking to supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'home_screen.dart';
 
-void main() {
+const supabaseUrl = 'https://oobuzeddpbkwmmxbriwm.supabase.co';
+const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vYnV6ZWRkcGJrd21teGJyaXdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyOTI4MzAsImV4cCI6MjAwNjg2ODgzMH0.qQE7CPTBe6UkSVAltGO8-54JRkELt5LUWMOx5armygk';
+
+Future<void> main() async {
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
   runApp(const MyApp());
 }
 
