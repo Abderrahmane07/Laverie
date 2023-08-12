648694be113d53cea53691864aa12f10c29bf8b4
selamam <mohammed.selama@grenoble-inp.org>
2023-08-12 18:25:30 +0200

    managing stacking machines.

---
+++ lib/models/machines-holder-model.dart
@@ -0,0 +1,40 @@
+import 'package:laverie/models/machine_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+/** this class corresponds to the table position in the database. */
+class MachinesHolder {
+  int position;
+  List<MachineModel>
+      stackedMachines; // this is an ordered list that contains stacked tables in one position.
+
+  MachinesHolder({required this.position, required this.stackedMachines});
+
+  Future<MachinesHolder> fromJson(Map<String, dynamic> json) async {
+    int position = json['position'];
+
+    List<MachineModel> machinesList =
+        await getMachinesFromIds(json['stacked_machines']);
+    return MachinesHolder(position: position, stackedMachines: machinesList);
+  }
+
+  Future<List<MachineModel>> getMachinesFromIds(
+      String stackedMachineIds) async {
+    List<String> machineIds = stackedMachineIds.split(',').toList();
+
+    // Assuming you have a method to fetch machines by ID from your database
+    List<MachineModel> machines = await fetchMachinesByIds(machineIds);
+
+    return machines;
+  }
+
+  Future<List<MachineModel>> fetchMachinesByIds(List<String> machineIds) async {
+    List<MachineModel> machines = [];
+
+    await Supabase.instance.client
+        .from('machines')
+        .select()
+        .in_('id', machineIds);
+
+    return machines;
+  }
+}
